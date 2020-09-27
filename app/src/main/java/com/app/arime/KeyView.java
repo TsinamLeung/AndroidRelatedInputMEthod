efe5c1a860f1fd391619022cad3b1edf21ab06ee
chinam <liangzinan21@hotmail.com>
2020-09-28 01:16:37 +0800

    [added]Gesture Detector To key Remove [Removed]Onclick Listener of KeyboardView

--- app/src/main/java/com/app/arime/KeyView.java
+++ app/src/main/java/com/app/arime/KeyView.java
@@ -4,17 +4,23 @@ import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.GestureDetector;
 import android.view.Gravity;
+import android.view.MotionEvent;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.view.GestureDetectorCompat;
 
 
 public class KeyView extends FrameLayout {
+    private static final String TAG = "arime.KeyView";
     private TextView hintText;
     private TextView keyText;
+    private GestureDetectorCompat gestureDetector;
 
     public KeyView(@NonNull Context context) {
         super(context);
@@ -35,9 +41,10 @@ public class KeyView extends FrameLayout {
         this.hintText.setGravity(Gravity.TOP);
         this.addView(hintText);
         this.addView(keyText);
+        gestureDetector = new GestureDetectorCompat(getContext(), new KeyboardGestureDetector());
     }
 
-    private int convertDP2PX(int dp) {
+    protected int convertDP2PX(int dp) {
         DisplayMetrics displayMetrics = this.getContext().getResources().getDisplayMetrics();
         return (int) ((dp * displayMetrics.density) + 0.5);
     }
@@ -56,5 +63,66 @@ public class KeyView extends FrameLayout {
 
     public void setKeyBorder(Drawable border) {
         this.setBackground(border);
+    }
+
+    public void showPopup() {
+        Log.d(TAG, "showPopup: ");
+    }
+
+    public void hidePopup() {
+        Log.d(TAG, "hidePopup: ");
+    }
+
+    public void showLongpress() {
+        Log.d(TAG, "showLongpress: ");
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_UP:
+                Log.d(TAG, "onTouchEvent: Action UP!!!");
+            default:
+                return gestureDetector.onTouchEvent(event);
+        }
+
+    }
+
+    protected class KeyboardGestureDetector implements GestureDetector.OnGestureListener {
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            showPopup();
+            return true;
+        }
+
+        @Override
+        public void onShowPress(MotionEvent e) {
+
+        }
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {
+            return false;
+        }
+
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            Log.d(TAG, "onScroll: start:" + e1.toString());
+            Log.d(TAG, "onScroll: distanceX " + distanceX + " DistanceY: " + distanceY);
+            Log.d(TAG, "onScroll: end: " + e2.toString());
+            return true;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+            showLongpress();
+        }
+
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            return false;
+        }
+
     }
 }
--- app/src/main/java/com/app/arime/KeyboardView.java
+++ app/src/main/java/com/app/arime/KeyboardView.java
@@ -3,14 +3,12 @@ package com.app.arime;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
-import android.view.View;
 
 import androidx.appcompat.widget.LinearLayoutCompat;
 
-public class KeyboardView extends LinearLayoutCompat implements View.OnClickListener {
+public class KeyboardView extends LinearLayoutCompat {
     private static final String TAG = "arime.KeyboardView";
+
 
     public KeyboardView(Context context) {
         super(context);
@@ -23,43 +21,6 @@ public class KeyboardView extends LinearLayoutCompat implements View.OnClickList
     }
 
     private void init() {
-
-    }
-
-    @Override
-    public void onClick(View v) {
-
-    }
-    public class Gesture implements GestureDetector.OnGestureListener {
-
-        @Override
-        public boolean onDown(MotionEvent e) {
-            return true;
-        }
-
-        @Override
-        public void onShowPress(MotionEvent e) {
-            Log.v("a","showpress");
-        }
-
-        @Override
-        public boolean onSingleTapUp(MotionEvent e) {
-            return true;
-        }
-
-        @Override
-        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-            return true;
-        }
-
-        @Override
-        public void onLongPress(MotionEvent e) {
-
-        }
-
-        @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-            return true;
-        }
+        Log.d(TAG, "Init()");
     }
 }
