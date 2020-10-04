c92d13159cdae575ac238cffe6021cbbf8378430
chinam <liangzinan21@hotmail.com>
2020-10-04 21:11:58 +0800

    [added] popup view style [issue]cannot use

--- app/src/main/java/com/app/arime/KeyView.java
+++ app/src/main/java/com/app/arime/KeyView.java
@@ -1,12 +1,14 @@
 package com.app.arime;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.GestureDetector;
 import android.view.Gravity;
 import android.view.MotionEvent;
@@ -26,7 +28,7 @@ public class KeyView extends FrameLayout {
     private TextView keyText;
     private GestureDetectorCompat gestureDetector;
     private PopupWindow popup;
-
+    private KeyPopup popupView;
 
     public KeyView(@NonNull Context context) {
         super(context);
@@ -47,6 +49,7 @@ public class KeyView extends FrameLayout {
         this.hintText.setGravity(Gravity.TOP);
         this.addView(hintText);
         this.addView(keyText);
+        popup = new PopupWindow();
         gestureDetector = new GestureDetectorCompat(getContext(), new KeyboardGestureDetector());
     }
 
@@ -57,6 +60,7 @@ public class KeyView extends FrameLayout {
 
     public void setKeyHeightAndWidth(int widthDP, int heightDP) {
         this.setLayoutParams(new LayoutParams(convertDP2PX(widthDP), convertDP2PX(heightDP)));
+        popupView = new KeyPopup(getContext(), convertDP2PX(widthDP), convertDP2PX(heightDP));
     }
 
     public final TextView getHintText() {
@@ -73,6 +77,9 @@ public class KeyView extends FrameLayout {
 
     public void showPopup() {
         Log.d(TAG, "showPopup: ");
+        // TODO: 20年10月4日 here big boom boom !!!! cannot use popup window damn
+        // popup.showAtLocation(popupView, Gravity.CENTER, (int) this.getX(), (int) this.getY());
+        Log.d(TAG, "showPopup: popup" + popup.toString());
     }
 
     public void hidePopup() {
@@ -95,16 +102,18 @@ public class KeyView extends FrameLayout {
 
     protected class KeyPopup extends LinearLayoutCompat {
 
-        private GradientDrawable border;
-        private GradientDrawable seletedBorder;
-        private FrameLayout showup;
+        private GradientDrawable keyBorder;
+        private GradientDrawable selectedBorder;
+        private FrameLayout hintContainer;
+        private int colorSelected;
         private TextView hintL;
         private TextView hintR;
         private TextView hintU;
         private TextView hintD;
         private TextView hintC;
-        private TextView selected;
+        private TextView hintSelected;
 
+        private boolean isLongPressed = false;
 
         public KeyPopup(@NonNull Context context, int keyWidth, int keyHeight) {
             super(context);
@@ -113,54 +122,163 @@ public class KeyView extends FrameLayout {
 
         private void init(int keyWidth, int keyHeight) {
             this.setLayoutParams(new LinearLayoutCompat.LayoutParams(keyWidth, keyHeight));
-            showup = new FrameLayout(getContext());
+            hintContainer = new FrameLayout(getContext());
             // Key hint of popup on top half of popup
-            showup.setLayoutParams(new FrameLayout.LayoutParams(keyWidth, keyHeight));
-            border = new GradientDrawable();
-            seletedBorder = new GradientDrawable();
-            this.setBackground(border);
+            hintContainer.setLayoutParams(new FrameLayout.LayoutParams(keyWidth, keyHeight));
+            keyBorder = new GradientDrawable();
+            selectedBorder = new GradientDrawable();
+            setupDefaultBorder(keyWidth, keyHeight);
+
+            this.setBackground(keyBorder);
             //init textView
-            hintL = new TextView(getContext());
-            hintR = new TextView(getContext());
-            hintU = new TextView(getContext());
-            hintD = new TextView(getContext());
-            hintC = new TextView(getContext());
-            selected = new TextView(getContext());
+            hintL = generateTextView();
+            hintR = generateTextView();
+            hintU = generateTextView();
+            hintD = generateTextView();
+            hintC = generateTextView();
+            initHintTextStyle();
+            hintContainer.addView(hintL);
+            hintContainer.addView(hintR);
+            hintContainer.addView(hintU);
+            hintContainer.addView(hintD);
+            hintContainer.addView(hintC);
+
+            hintSelected = generateTextView();
+            hintSelected.setVisibility(INVISIBLE);
+            this.addView(hintSelected);
         }
 
-        protected void setHintStyle(TextView tv, int tSize, int tUnit, int textColor, @Nullable Typeface tf) {
+        /**
+         * @param width
+         * @param height ________         ____________
+         *               | PopUp |        |Some Color |
+         *               |       |    ->  |  Pressed  |
+         *               |_______|        |___________|
+         *               ______
+         *               | key |
+         *               -------
+         */
+        protected void setupDefaultBorder(int width, int height) {
+            keyBorder.setSize(width, height);
+            selectedBorder.setSize(width, height);
+            setKeyBorderStyle(0xFF242424, convertDP2PX(6));
+            setSelectedBorderStyle(0xFF0002BE);
+        }
+
+        /**
+         * generate default sytle of hint textview
+         *
+         * @return TextView
+         */
+        private TextView generateTextView() {
+            TextView tv = new TextView(getContext());
+            tv.setGravity(Gravity.CENTER);
+            FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.MATCH_PARENT);
+            params.gravity = Gravity.CENTER;
+            tv.setLayoutParams(params);
+            setHintStyle(tv, Color.WHITE, 1.0f, null);
+            return tv;
+        }
+
+        private void initHintTextStyle() {
+            /** set hint width and height and  layout is somehow like
+             *      ____________________
+             *     |        Up          |
+             *     |____________________|
+             *     |    |            | R|
+             *     | L  |            | I|
+             *     | E  |   CENTER   | G|
+             *     | F  |            | H|
+             *     | T  |            | T|
+             *     |____|____________|__|
+             *     |         Down       |
+             *     |____________________|
+             */
+            FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) hintU.getLayoutParams();
+            params.gravity |= Gravity.TOP;
+            params.height = hintContainer.getHeight() / 4;
+            hintU.setLayoutParams(params);
+            hintU.setAlpha(0.8f);
+
+            params = (FrameLayout.LayoutParams) hintD.getLayoutParams();
+            params.gravity |= Gravity.BOTTOM;
+            params.height = hintContainer.getHeight() / 4;
+            hintD.setLayoutParams(params);
+            hintD.setAlpha(0.8f);
+
+            params = (FrameLayout.LayoutParams) hintL.getLayoutParams();
+            params.gravity |= Gravity.LEFT;
+            params.width = hintContainer.getWidth() / 4;
+            params.height = hintContainer.getHeight() / 2;
+            hintL.setLayoutParams(params);
+            hintL.setAlpha(0.8f);
+
+            params = (FrameLayout.LayoutParams) hintR.getLayoutParams();
+            params.gravity |= Gravity.RIGHT;
+            params.width = hintContainer.getWidth() / 4;
+            params.height = hintContainer.getHeight() / 2;
+            hintR.setLayoutParams(params);
+            hintR.setAlpha(0.8f);
+
+            params = (FrameLayout.LayoutParams) hintC.getLayoutParams();
+            params.width = hintContainer.getWidth() / 2;
+            params.height = hintContainer.getHeight() / 2;
+            hintC.setLayoutParams(params);
+
+        }
+
+        protected void setHintStyle(TextView tv, int textColor, float alpha, @Nullable Typeface tf) {
             tv.setTypeface(tf);
             tv.setTextColor(textColor);
-            tv.setTextSize(tSize, tUnit);
-            tv.setAutoSizeTextTypeUniformWithConfiguration(tSize / 2, tSize, TextView.AUTO_SIZE_TEXT_TYPE_UNIFORM, tUnit);
+            tv.setAlpha(alpha);
+            tv.setAutoSizeTextTypeUniformWithConfiguration(8, 100, TextView.AUTO_SIZE_TEXT_TYPE_UNIFORM, TypedValue.COMPLEX_UNIT_DIP);
         }
 
-        public void setBorderStyle(int backgroundColor, float raidusPX) {
-            border.setSize(this.getWidth(), this.getHeight());
-            border.setCornerRadius(raidusPX);
-            border.setColor(backgroundColor);
+        public void setKeyBorderStyle(int backgroundColor, float raidusPX) {
+            keyBorder.setSize(this.getWidth(), this.getHeight());
+            keyBorder.setCornerRadius(raidusPX);
+            keyBorder.setColor(backgroundColor);
         }
 
-        public void setSeletedStyle(int seletedColor) {
-            seletedBorder.setCornerRadius(border.getGradientRadius());
-            seletedBorder.setColor(seletedColor);
-            seletedBorder.setVisible(false, false);
+        public void setSelectedBorderStyle(int backgroundColor) {
+            selectedBorder.setSize(this.getWidth(), this.getHeight());
+            selectedBorder.setCornerRadius(keyBorder.getCornerRadius());
+            selectedBorder.setColor(backgroundColor);
+            colorSelected = backgroundColor;
+        }
+
+        public void setHintStyle(int textColor, @Nullable Typeface tf) {
+            setHintStyle(hintC, textColor, 1.0f, tf);
+            setHintStyle(hintL, textColor, 0.8f, tf);
+            setHintStyle(hintR, textColor, 0.8f, tf);
+            setHintStyle(hintU, textColor, 0.8f, tf);
+            setHintStyle(hintD, textColor, 0.8f, tf);
+            setHintStyle(hintSelected, 0xFF000000 | ((~(colorSelected << 2) + 1) >> 2), 1.0f, tf);
         }
 
         /**
          * for popup key hint
          */
         public void setHint(String center, String left, String right, String up, String down) {
-            boolean isSingle = !(left.isEmpty() | right.isEmpty() | up.isEmpty() | down.isEmpty());
-            if (isSingle) {
-
-            } else {
-
-            }
+            hintC.setText(center);
+            hintL.setText(left);
+            hintR.setText(right);
+            hintU.setText(up);
+            hintD.setText(down);
         }
 
         public void longPressed() {
-            // TODO: 20年10月4日 show seleted after long pressed
+            hintContainer.setVisibility(INVISIBLE);
+            hintSelected.setVisibility(VISIBLE);
+            this.setBackground(selectedBorder);
+            isLongPressed = true;
+        }
+
+        protected void resetView() {
+            hintContainer.setVisibility(VISIBLE);
+            hintSelected.setVisibility(INVISIBLE);
+            this.setBackground(keyBorder);
+            isLongPressed = false;
         }
     }
 
